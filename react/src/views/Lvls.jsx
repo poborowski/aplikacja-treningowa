ccb170fe12d3474f696416bbc33a84e30ef4a7e5
poborowski <bpoborowski@gmail.com>
2023-05-14 13:32:07 +0200

    feat: lvls, workouts, list

--- react/src/views/Lvls.jsx
+++ react/src/views/Lvls.jsx
@@ -1,23 +1,33 @@
 import {Link} from "react-router-dom";
+import React, { useState } from 'react';
 import Header from "../components/Header.jsx";
+function ChooseOptionBox() {
+  const [options, setOptions] = useState([
+    { id: 1, label: 'Początkujący', link: '/' },
+    { id: 2, label: 'Średni', link: '/' },
+    { id: 3, label: 'Zaawansowany', link: '/' },
+  ]);
+
+  return (
+    <div className="chooseOptionBox">
+      {options.map((option) => (
+        <div key={option.id} className="choosenBox">
+          <Link to={option.link}>{option.label}</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default function Lvls() {
-  return (<div>
-    <Header />
-    <div className="headingBox">
-      <p>Wybierz poziom trudności ćwiczeń</p>
+  return (
+    <div>
+      <Header />
+      <div className="headingBox">
+        <p>Wybierz poziom trudności ćwiczeń</p>
+      </div>
+      <ChooseOptionBox />
+      <img className="imgFloatRight" src="../src/assets/men.png" alt="Logo" />
     </div>
-    <div className="chooseOptionBox">
-      <div className="choosenBox">
-        <Link to="/">Początkujący</Link>
-      </div>
-      <div className="choosenBox">
-        <Link to="/">Średni</Link>
-      </div>
-      <div className="choosenBox">
-        <Link to="/">Zaawansowany</Link>
-      </div>
-    </div>
-    <img className="imgFloatRight" src={"../src/assets/men.png"} alt="Logo"/>
-  </div>)
+  );
 }
--- react/src/views/Workouts.jsx
+++ react/src/views/Workouts.jsx
@@ -1,23 +1,34 @@
+
 import {Link} from "react-router-dom";
+import React, { useState } from 'react';
 import Header from "../components/Header.jsx";
+function ChooseWorkoutsBox() {
+  const [options, setOptions] = useState([
+    { id: 1, label: 'Klata', link: '/' },
+    { id: 2, label: 'Plecy', link: '/' },
+    { id: 3, label: 'Nogi', link: '/' },
+  ]);
+
+  return (
+    <div className="chooseOptionBox">
+      {options.map((option) => (
+        <div key={option.id} className="choosenBox">
+          <Link to={option.link}>{option.label}</Link>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default function Workouts() {
-  return (<div>
-    <Header />
-    <div className="headingBox">
-      <p>Wybierz partię ciała którą chcesz ćwiczyć</p>
+  return (
+    <div>
+      <Header />
+      <div className="headingBox">
+        <p>Wybierz co chcesz dziś ćwiczyć</p>
+      </div>
+      <ChooseWorkoutsBox />
+      <img className="imgFloatRight" src="../src/assets/men.png" alt="Logo" />
     </div>
-    <div className="chooseOptionBox">
-      <div className="choosenBox">
-        <Link to="/">Klata</Link>
-      </div>
-      <div className="choosenBox">
-        <Link to="/">Plecy</Link>
-      </div>
-      <div className="choosenBox">
-        <Link to="/">Nogi</Link>
-      </div>
-    </div>
-    <img className="imgFloatRight" src={"../src/assets/men.png"} alt="Logo"/>
-  </div>)
+  );
 }
