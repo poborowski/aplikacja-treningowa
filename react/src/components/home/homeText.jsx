ed68c908474d5e19b80a28f33cd8af287c5e9fc7
poborowski <bpoborowski@gmail.com>
2023-05-16 00:20:56 +0200

    feat: home components

---
+++ react/src/components/home/homeText.jsx
@@ -0,0 +1,18 @@
+
+function HomeText() {
+
+  return (
+    <div className="col-6">
+      <p className={"bigHeading"}>Dlaczego warto
+        do nas dołączyć?</p>
+      <p>Nasza siłownia oferuje nie tylko doskonałe wyposażenie,
+        ale także profesjonalne wsparcie. Nasi doświadczeni
+        trenerzy są gotowi doradzić i odpowiedzieć na wszystkie
+        pytania, abyś mógł/mogła trenować z pełnym
+        zaangażowaniem i bezpieczeństwem.</p>
+    </div>
+  );
+}
+
+export default HomeText;
+
--- react/src/views/Home.jsx
+++ react/src/views/Home.jsx
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom";
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
+import HomeText from "../components/home/homeText.jsx";
 
 export default function Home() {
   return (
@@ -9,15 +10,7 @@ export default function Home() {
       <div className="homeHero">
         <div className="container">
           <div className="row">
-            <div className="col-6">
-              <p className={"bigHeading"}>Dlaczego warto
-                do nas dołączyć?</p>
-              <p>Nasza siłownia oferuje nie tylko doskonałe wyposażenie,
-                ale także profesjonalne wsparcie. Nasi doświadczeni
-                trenerzy są gotowi doradzić i odpowiedzieć na wszystkie
-                pytania, abyś mógł/mogła trenować z pełnym
-                zaangażowaniem i bezpieczeństwem.</p>
-            </div>
+            <HomeText/>
             <div className="col-6"><img src={"../src/assets/man2.png"} alt="Logo"/></div>
           </div>
         </div>
@@ -27,15 +20,7 @@ export default function Home() {
         <div className="container">
           <div className="row">
             <div className="col-6"><img src={"../src/assets/man3.png"} alt="Logo"/></div>
-            <div className="col-6">
-              <p className={"bigHeading"}>Dlaczego warto
-                do nas dołączyć?</p>
-              <p>Nasza siłownia oferuje nie tylko doskonałe wyposażenie,
-                ale także profesjonalne wsparcie. Nasi doświadczeni
-                trenerzy są gotowi doradzić i odpowiedzieć na wszystkie
-                pytania, abyś mógł/mogła trenować z pełnym
-                zaangażowaniem i bezpieczeństwem.</p>
-            </div>
+          <HomeText/>
 
           </div>
         </div>
@@ -43,15 +28,7 @@ export default function Home() {
       <div className="textImageHomeWrap">
         <div className="container">
           <div className="row">
-            <div className="col-6">
-              <p className={"bigHeading"}>Dlaczego warto
-                do nas dołączyć?</p>
-              <p>Nasza siłownia oferuje nie tylko doskonałe wyposażenie,
-                ale także profesjonalne wsparcie. Nasi doświadczeni
-                trenerzy są gotowi doradzić i odpowiedzieć na wszystkie
-                pytania, abyś mógł/mogła trenować z pełnym
-                zaangażowaniem i bezpieczeństwem.</p>
-            </div>
+            <HomeText/>
             <div className="col-6"><img src={"../src/assets/ma4.png"} alt="Logo"/></div>
           </div>
         </div>
